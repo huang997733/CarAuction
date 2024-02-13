1adcaef4ae8a8a838bb31c076c8e3b89c49e6e2a
huang997733 <williamhzy99@gmail.com>
2024-02-13 22:19:50 +1100

    add search microservice

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(a => a.Make, KeyType.Text)
+            .Key(a => a.Model, KeyType.Text)
+            .Key(a => a.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " items received from AuctionService");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+    }
+}

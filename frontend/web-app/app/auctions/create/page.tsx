63d950be257caf370d762f7f64e1273b8e75f4f2
huang997733 <williamhzy99@gmail.com>
2024-03-02 17:13:22 +1100

    add a create auction form using react hook form

---
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -0,0 +1,15 @@
+import Heading from "@/app/components/Heading";
+import React from "react";
+import AuctionForm from "../AuctionForm";
+
+export default function Create() {
+  return (
+    <div className="mx-auto max-w-[75] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Sell your car!"
+        subtitle="Please fill in the details of your car to create an auction."
+      />
+      <AuctionForm />
+    </div>
+  );
+}

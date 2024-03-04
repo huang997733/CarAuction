d69d98c95c1daf0b0a3f98fbfdc81470a0a1d956
huang997733 <williamhzy99@gmail.com>
2024-03-04 17:38:18 +1100

    implement bidding service

---
+++ src/BiddingService/Services/CheckAuctionFinished.cs
@@ -0,0 +1,69 @@
+﻿
+
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace BiddingService;
+
+public class CheckAuctionFinished : BackgroundService
+{
+    private readonly ILogger<CheckAuctionFinished> _logger;
+    private readonly IServiceProvider _services;
+
+    public CheckAuctionFinished(ILogger<CheckAuctionFinished> logger, IServiceProvider services)
+    {
+        _logger = logger;
+        _services = services;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("CheckAuctionFinished is starting.");
+
+        stoppingToken.Register(() =>
+            _logger.LogInformation("CheckAuctionFinished background task is stopping."));
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await CheckAuction(stoppingToken);
+
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+
+    private async Task CheckAuction(CancellationToken stoppingToken)
+    {
+        var finishedAuctions = await DB.Find<Auction>()
+            .Match(a => a.AuctionEnd <= DateTime.UtcNow)
+            .Match(a => !a.Finished)
+            .ExecuteAsync(stoppingToken);
+
+        if (finishedAuctions.Count == 0) return;
+
+        _logger.LogInformation("Found {count} finished auctions.", finishedAuctions.Count);
+
+        using var scope = _services.CreateScope();
+        var endpoint = scope.ServiceProvider.GetRequiredService<IPublishEndpoint>();
+
+        foreach (var auction in finishedAuctions)
+        {
+            auction.Finished = true;
+            await auction.SaveAsync(null, stoppingToken);
+
+            var winningBid = await DB.Find<Bid>()
+                .Match(b => b.AuctionId == auction.ID)
+                .Match(b => b.Amount >= auction.ReservePrice)
+                .Sort(x => x.Descending(s => s.Amount))
+                .ExecuteFirstAsync(stoppingToken);
+
+            await endpoint.Publish(new AuctionFinished
+            {
+                ItemSold = winningBid != null,
+                AuctionId = auction.ID,
+                Winner = winningBid?.Bidder,
+                Amount = winningBid?.Amount,
+                Seller = auction.Seller
+            }, stoppingToken);
+        }
+    }
+}
